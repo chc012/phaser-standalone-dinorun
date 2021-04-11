e6006b7e457834b43cfd24b525484bbe40ac0756
chc012 <chc012@ucsd.edu>
2021-04-11 14:43:26 -0700

    Add scores, speed

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -16,11 +16,14 @@ export class GameScene extends Phaser.Scene {
   private ptera: Phaser.Physics.Arcade.Group;
   private cloud: Phaser.Physics.Arcade.Group;
   
-  private speed = 7;
+  private speed = -200;
   private score = 0;
+  private frame = 0;
+  private highScore = 300;
+  private scoreText: Phaser.GameObjects.Text;
+  private highScoreText: Phaser.GameObjects.Text;
 
   private cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
-  private scoreText: Phaser.GameObjects.Text;
 
   constructor() {
     super(sceneConfig);
@@ -41,9 +44,9 @@ export class GameScene extends Phaser.Scene {
       getGameHeight(this) * 0.975,
       "ground"
     );
-    this.ground.setSize(
+    this.ground.setDisplaySize(
       getGameWidth(this),
-      getGameHeight(this) * 0.05
+      getGameHeight(this) * 0.1 
     );
     this.ground.setCollideWorldBounds(true);
 
@@ -66,18 +69,27 @@ export class GameScene extends Phaser.Scene {
     this.ptera = this.physics.add.group({allowGravity: false});
     
     this.scoreText = this.add.text(
-      getGameWidth(this) * 0.88,
+      getGameWidth(this) * 0.98,
       getGameHeight(this) * 0.08,
-      "00000",
+      "S:".concat("0"),
       {fontSize: "20px", color: "#000"}
     );
+    this.scoreText.setOrigin(1, 0);
+
+    this.highScoreText = this.add.text(
+      getGameWidth(this) * 0.8,
+      getGameHeight(this) * 0.08,
+      "H:".concat(this.highScore.toString()),
+      {fontSize: "20px", color: "#000"}
+    );
+    this.highScoreText.setOrigin(1, 0);
     
     // Animation for dino and ptera
     this.anims.create({
       key: "run",
       frames: this.anims.generateFrameNumbers(
         "dino",
-        {start: 0, end: 4}
+        {start: 2, end: 3}
       ),
       frameRate: 10,
       repeat: -1
@@ -103,12 +115,22 @@ export class GameScene extends Phaser.Scene {
       repeat: -1
     });
 
+    this.anims.create({
+      key: "die",
+      frames: [{key: "dino", frame: 4}],
+      frameRate: 20
+    });
+
     // Collision effect
     this.physics.add.collider(this.dino, this.cacti, hit, null, this);
     this.physics.add.collider(this.dino, this.ptera, hit, null, this);
 
+    // Setting end-game event
+    // TODO
     function hit(dino: Phaser.Physics.Arcade.Sprite, obstacle) {
-      return
+      this.physics.pause()
+      dino.anims.play("die");
+      this.gameOver = true;
     }
 
     this.cursorKeys = this.input.keyboard.createCursorKeys();
@@ -116,7 +138,8 @@ export class GameScene extends Phaser.Scene {
 
   public update(): void {
 
-    if (this.cloud.getLength() < 3
+    // Clouds
+    if (this.cloud.getLength() < 5
         && Phaser.Math.FloatBetween(0, 1) < 0.01) {
       let new_cloud: Phaser.Physics.Arcade.Sprite = this.cloud.create(
         getGameWidth(this),
@@ -136,6 +159,7 @@ export class GameScene extends Phaser.Scene {
       }
     });
 
+    // Ptera
     if (this.ptera.getLength() < 1
         && Phaser.Math.FloatBetween(0, 1) < 0.005) {
       let new_ptera: Phaser.Physics.Arcade.Sprite = this.ptera.create(
@@ -143,12 +167,14 @@ export class GameScene extends Phaser.Scene {
         getGameHeight(this) * Phaser.Math.FloatBetween(0.2, 0.6),
         "ptera"
       );
-      new_ptera.setVelocityX(-150);
+      new_ptera.setVelocityX(this.speed);
       new_ptera.setOrigin(0, 0);
       new_ptera.setDisplaySize(
         getGameWidth(this) * 0.06,
         getGameHeight(this) * 0.24 * 0.88
       );
+      new_ptera.setDepth(1);
+      new_ptera.anims.play("fly", true); 
     }
     this.ptera.children.each(function (child: Phaser.Physics.Arcade.Sprite) {
       if (child.getRightCenter().x <=0) {
@@ -156,14 +182,16 @@ export class GameScene extends Phaser.Scene {
       }
     });
 
+    // Cacti
     if (this.cacti.getLength() < 2
         && Phaser.Math.FloatBetween(0, 1) < 0.01) {
       let new_cactus: Phaser.Physics.Arcade.Sprite = this.cacti.create(
         getGameWidth(this),
         getGameHeight(this),
-        "cacti"
+        "cacti",
+
       );
-      new_cactus.setVelocityX(-150);
+      new_cactus.setVelocityX(this.speed);
       new_cactus.setOrigin(0, 1);
       new_cactus.setDisplaySize(
         getGameWidth(this) * 0.06,
@@ -176,11 +204,32 @@ export class GameScene extends Phaser.Scene {
       }
     });
 
-    // Animation is failing me, think about it later
-
+    // Dino
+    if (this.dino.texture.key == "dino") {
+      this.dino.anims.play("run", true);
+    } else {
+      this.dino.anims.play("duck_run", true);
+    }
     if ((this.cursorKeys.up.isDown || this.cursorKeys.space.isDown)
         && (this.dino.body.bottom === getGameHeight(this))) {
       this.dino.setVelocityY(-450);
+    }
+
+    // Score
+    this.frame++;
+    if (this.frame % 6 === 0) {
+      this.score++;
+      this.scoreText.setText("S:".concat(this.score.toString()));
+      if (this.score >= this.highScore) {
+        this.highScore = this.score;
+        this.highScoreText.setText("H:".concat(this.highScore.toString()));
+      }
+    }
+
+    // Speed change
+    if (this.frame % 600 === 0) {
+      console.log(this.frame);
+      this.speed -= 50;
     }
 
     // Check jump and duck
@@ -205,6 +254,5 @@ export class GameScene extends Phaser.Scene {
         getGameHeight(this) * 0.6
       );
     }
-
   }
 }
